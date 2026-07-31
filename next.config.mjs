186f66221283a84d28b9032846e9e47af6356a25
iqra choudhary <iqraahsan262969@gmail.com>
2026-07-31 07:05:28 +0500

    chore: force vercel rebuild

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  // Force rebuild for Vercel 1
 };
 
 export default nextConfig;
